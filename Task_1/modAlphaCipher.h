7b6e4c4078199192fa036454f34d97b0787084d8
stanchin04 <145608158+stanchin04@users.noreply.github.com>
2024-01-09 16:55:12 +0300

    Create modAlphaCipher.h

---
+++ Task_1/modAlphaCipher.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <locale>
+#include <codecvt>
+#include <vector>
+#include <map>
+using namespace std;
+class modAlphaCipher
+{
+private:
+    string numAlpha =
+        "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"; //алфавит по порядку
+    map <char,int> alphaNum; //ассоциативный массив "номер по символу"
+    vector <int> key; //ключ
+    vector<int> convert(const string& s); //преобразование строка-вектор
+    string convert(const vector<int>& v); //преобразование вектор-строка
+public:
+    modAlphaCipher()=delete; //запретим конструктор без параметров
+    modAlphaCipher(const string& skey); //конструктор для установки ключа
+    string encrypt(const string& open_text); //зашифрование
+    string decrypt(const string& cipher_text);//расшифрование
+};
